9fe5edd9369bf33d8d17d558e0dd414818988d22
nahidistiak <nahid.istiak@gmail.com>
2019-07-21 13:23:16 +0600

    init attendance report

--- src/components/common/AttendanceReport.js
+++ src/components/common/AttendanceReport.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
+import AttendanceReportForm from "./AttendanceReportForm";
+import ShowAttendanceReport from "./ShowAttendanceReport";
 
-export default props => {
-  return <h1>Attendance Report</h1>;
-};
+class AttendanceReport extends React.Component {
+  state = {
+    formOpen: true,
+    selections: {
+      class: "",
+      subject: "",
+      session: "",
+      from: "",
+      to: ""
+    }
+  };
+
+  handleClick = () => this.setState({ formOpen: !this.state.formOpen });
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col>
+            {this.state.formOpen ? (
+              <AttendanceReportForm onClick={this.handleClick} {...this.state.selections} />
+            ) : (
+              <ShowAttendanceReport onClick={this.handleClick} {...this.state.selections} />
+            )}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default AttendanceReport;
