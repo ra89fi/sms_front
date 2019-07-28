9343ef21f85af6ab232241e4b6581181502d986d
nahidistiak <nahid.istiak@gmail.com>
2019-07-28 13:36:37 +0600

    exam marks entry handling

--- src/components/exam/ExamDetails.js
+++ src/components/exam/ExamDetails.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, CardHeader, CardBody, Col, Row, Table } from "reactstrap";
 
+const exam = {
+  id: "290674289367",
+  degree: "Honors",
+  subject: "Geography",
+  session: "2019-20",
+  date: "7/12/2019",
+  name: "Exam Name",
+  description: "Description",
+  marks: {}
+};
+
 export default props => {
   return (
     <div className="animated fadeIn">
@@ -14,41 +25,56 @@ export default props => {
             <CardBody>
               <Row>
                 <Col>Degree</Col>
-                <Col>: Honors</Col>
+                <Col>
+                  : <strong>{exam.degree}</strong>
+                </Col>
               </Row>
               <Row>
                 <Col>Subject</Col>
-                <Col>: Geography</Col>
+                <Col>
+                  : <strong>{exam.subject}</strong>
+                </Col>
               </Row>
               <Row>
                 <Col>Session</Col>
-                <Col>: 2019-20</Col>
+                <Col>
+                  : <strong>{exam.session}</strong>
+                </Col>
               </Row>
               <Row>
                 <Col>Date</Col>
-                <Col>: 7/12/2019</Col>
+                <Col>
+                  : <strong>{exam.date}</strong>
+                </Col>
               </Row>
               <Row>
                 <Col>Exam Name</Col>
-                <Col>: Exam Name</Col>
+                <Col>
+                  : <strong>{exam.name}</strong>
+                </Col>
               </Row>
               <Row>
                 <Col>Exam Description</Col>
-                <Col>: Exam Description</Col>
+                <Col>
+                  : <strong>{exam.description}</strong>
+                </Col>
               </Row>
             </CardBody>
           </Card>
           <Row>
             <Col>
-              <Link to="/exams/entry/123">
+              <Link to={`/exams/entry/${exam.id}`}>
                 <Button color="warning">Edit Exam</Button>
               </Link>
               &nbsp;
-              <Button color="danger">Delete Exam</Button>
-              &nbsp;
-              <Link to="/exams/marks/123">
-                <Button color="primary">Entry marks</Button>
-              </Link>
+              <Button color="danger">Delete Exam</Button>&nbsp;
+              {Object.keys(exam.marks).length ? (
+                <Link to={`/exams/marks/${exam.id}`}>
+                  <Button color="primary">Entry marks</Button>
+                </Link>
+              ) : (
+                ""
+              )}
             </Col>
           </Row>
         </Col>
@@ -73,24 +99,6 @@ export default props => {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>11152622</td>
-                    <td>saumik</td>
-                    <td>30</td>
-                    <td>Excellent</td>
-                  </tr>
-                  <tr>
-                    <td>11152622</td>
-                    <td>saumik</td>
-                    <td>30</td>
-                    <td>Excellent</td>
-                  </tr>
-                  <tr>
-                    <td>11152622</td>
-                    <td>saumik</td>
-                    <td>30</td>
-                    <td>Excellent</td>
-                  </tr>
                   <tr>
                     <td>11152622</td>
                     <td>saumik</td>
--- src/components/exam/ExamMarks.js
+++ src/components/exam/ExamMarks.js
@@ -2,119 +2,186 @@ import React from "react";
 import { Button, Card, CardHeader, CardBody, Col, Row, Table } from "reactstrap";
 import FormField from "../common/FormField";
 
-export default props => {
-  return (
-    <div className="animated fadeIn">
-      <Row>
-        <Col>
-          <Card>
-            <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
-              <strong>Entry Marks</strong> <i className="icon-note icons" />
-            </CardHeader>
-            <CardBody>
-              <Row>
-                <Col xs="4">
-                  <Row>
-                    <Col>Degree</Col>
-                    <Col>: Honors</Col>
-                  </Row>
-                  <Row>
-                    <Col>Subject</Col>
-                    <Col>: Geogrpahy</Col>
-                  </Row>
-                  <Row>
-                    <Col>Session</Col>
-                    <Col>: 2019-20</Col>
-                  </Row>
-                  <Row>
-                    <Col>Exam Name</Col>
-                    <Col>: Tutorial 1</Col>
-                  </Row>
-                  <Row>
-                    <Col>Exam Description</Col>
-                    <Col>: Exam Description</Col>
-                  </Row>
-                  <Row>
-                    <Col>Date</Col>
-                    <Col>: 7/12/2019</Col>
-                  </Row>
-                </Col>
-                <Col />
-              </Row>
-              <Row>
-                <Col style={{ minHeight: "30px" }} />
-              </Row>
-              <Row>
-                <Col>
-                  <Table responsive>
-                    <thead>
-                      <tr>
-                        <th>Roll No</th>
-                        <th>Student Name</th>
-                        <th>Marks</th>
-                        <th>Notes</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>11152622</td>
-                        <td>saumik</td>
-                        <td className="removeMarginBottom">
-                          <FormField type="text" placeholder="Marks" />
-                        </td>
-                        <td className="removeMarginBottom">
-                          <FormField placeholder="Notes" type="text" />
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td>11152622</td>
-                        <td>saumik</td>
-                        <td className="removeMarginBottom">
-                          <FormField type="text" placeholder="Marks" />
-                        </td>
-                        <td className="removeMarginBottom">
-                          <FormField placeholder="Notes" type="text" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>11152622</td>
-                        <td>saumik</td>
-                        <td className="removeMarginBottom">
-                          <FormField type="text" placeholder="Marks" />
-                        </td>
-                        <td className="removeMarginBottom">
-                          <FormField placeholder="Notes" type="text" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>11152622</td>
-                        <td>saumik</td>
-                        <td className="removeMarginBottom">
-                          <FormField type="text" placeholder="Marks" />
-                        </td>
-                        <td className="removeMarginBottom">
-                          <FormField placeholder="Notes" type="text" />
-                        </td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Col>
-              </Row>
-              <Row>
-                <Col style={{ minHeight: "20px" }} />
-              </Row>
-              <Row>
-                <Col>
-                  <Button block color="primary">
-                    Submit
-                  </Button>
-                </Col>
-              </Row>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
+const exam = {
+  id: "290674289367",
+  degree: "Honors",
+  subject: "Geography",
+  session: "2019-20",
+  date: "7/12/2019",
+  name: "Exam Name",
+  description: "Description",
+  marks: {},
+  notes: {}
 };
+
+class ExamMarks extends React.Component {
+  state = {
+    total: "",
+    highest: "",
+    marks: {
+      11152622: ""
+    },
+    notes: {
+      11152622: ""
+    }
+  };
+
+  changeHandler = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+
+  updateMarks = (e, id) => {
+    this.setState({
+      marks: {
+        ...this.state.marks,
+        [id]: e.target.value
+      }
+    });
+  };
+
+  updateNotes = (e, id) => {
+    this.setState({
+      notes: {
+        ...this.state.notes,
+        [id]: e.target.value
+      }
+    });
+  };
+
+  submitHandler = () => {
+    console.log(this.state);
+    // validate and send to server
+  };
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
+                <strong>Entry Marks</strong> <i className="icon-note icons" />
+              </CardHeader>
+              <CardBody>
+                <Row>
+                  <Col xs="4">
+                    <Row>
+                      <Col>Degree</Col>
+                      <Col>
+                        : <strong>{exam.degree}</strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>Subject</Col>
+                      <Col>
+                        : <strong>{exam.subject}</strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>Session</Col>
+                      <Col>
+                        : <strong>{exam.session}</strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>Date</Col>
+                      <Col>
+                        : <strong>{exam.date}</strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>Exam Name</Col>
+                      <Col>
+                        : <strong>{exam.name}</strong>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>Exam Description</Col>
+                      <Col>
+                        : <strong>{exam.description}</strong>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col />
+                </Row>
+                <Row style={{ marginTop: "20px" }}>
+                  <Col>
+                    <FormField
+                      type="text"
+                      placeholder="Total Marks"
+                      name="total"
+                      value={this.state.total}
+                      onChange={this.changeHandler}
+                    />
+                  </Col>
+                  <Col>
+                    <FormField
+                      type="text"
+                      placeholder="Highest"
+                      name="highest"
+                      value={this.state.value}
+                      onChange={this.changeHandler}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Table responsive>
+                      <thead>
+                        <tr>
+                          <th>Roll No</th>
+                          <th>Student Name</th>
+                          <th>Obtained</th>
+                          <th>Notes</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>11152622</td>
+                          <td>saumik</td>
+                          <td className="removeMarginBottom">
+                            <FormField
+                              type="text"
+                              placeholder="Marks"
+                              name="11152622"
+                              value={this.state.marks["11152622"]}
+                              onChange={e => this.updateMarks(e, "11152622")}
+                            />
+                          </td>
+                          <td className="removeMarginBottom">
+                            <FormField
+                              placeholder="Notes"
+                              type="text"
+                              name="11152622"
+                              value={this.state.notes["11152622"]}
+                              onChange={e => this.updateNotes(e, "11152622")}
+                            />
+                          </td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col style={{ minHeight: "20px" }} />
+                </Row>
+                <Row>
+                  <Col />
+                  <Col>
+                    <Button block color="primary" onClick={this.submitHandler}>
+                      Submit
+                    </Button>
+                  </Col>
+                  <Col />
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ExamMarks;
