b94307e98de82a8d1d195af7bb9b15608bd47826
nahidistiak <nahid.istiak@gmail.com>
2019-07-28 12:31:13 +0600

    refactor exam list to use list component

--- src/components/exam/ExamList.js
+++ src/components/exam/ExamList.js
@@ -1,6 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Card, CardHeader, CardBody, Col, Row, Table } from "reactstrap";
+import { Card, CardHeader, CardBody, Col, Row } from "reactstrap";
+import List from "../common/List";
+
+const examData = [
+  {
+    id: "29067429867473",
+    degree: "Honors",
+    subject: "Geography",
+    session: "2013-14",
+    name: "Exam Name",
+    description: "Description",
+    date: "12/7/2019"
+  }
+];
+const buttons = [
+  {
+    name: "View",
+    color: "success",
+    link: "/exams/details"
+  },
+  {
+    name: "Edit",
+    color: "warning",
+    link: "/exams/entry"
+  },
+  {
+    name: "Delete",
+    color: "danger"
+  },
+  {
+    name: "Marks",
+    color: "primary",
+    link: "/exams/marks"
+  }
+];
 
 export default props => {
   return (
@@ -12,207 +45,7 @@ export default props => {
               <strong>Exam List</strong> <i className="icon-list icons" />
             </CardHeader>
             <CardBody>
-              <Table responsive>
-                <thead>
-                  <tr>
-                    <th>Degree</th>
-                    <th>Subject</th>
-                    <th>Session</th>
-                    <th>Exam Name</th>
-                    <th>Exam Description</th>
-                    <th>Date</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Honors</td>
-                    <td>Geography</td>
-                    <td>2013-14</td>
-                    <td>11152622</td>
-                    <td>62389</td>
-                    <td>saumik</td>
-                    <td>
-                      <Link to="/exams/details/123">
-                        <Button color="success" size="sm" className="btn-pill">
-                          View
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Link to="/exams/entry/123">
-                        <Button color="warning" size="sm" className="btn-pill">
-                          Edit
-                        </Button>
-                      </Link>
-                      &nbsp;
-                      <Button color="danger" size="sm" className="btn-pill">
-                        Delete
-                      </Button>
-                      &nbsp;
-                      <Link to="/exams/marks/123">
-                        <Button color="primary" size="sm" className="btn-pill">
-                          Entry Marks
-                        </Button>
-                      </Link>
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
+              <List data={examData} buttons={buttons} />
             </CardBody>
           </Card>
         </Col>
