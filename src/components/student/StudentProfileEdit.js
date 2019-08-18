32eef04167682cba5e8c6444ebb2d2614d626707
nahidistiak <nahid.istiak@gmail.com>
2019-08-18 18:09:33 +0600

    student profile edit connect to back end

--- src/components/student/StudentProfileEdit.js
+++ src/components/student/StudentProfileEdit.js
@@ -5,6 +5,8 @@ import Joi from "@hapi/joi";
 import StudentDetails from "./StudentDetails";
 import { updateStudentDetails, resetStudent } from "../../actions/student";
 import { studentDetailsSchema } from "../../validations/student";
+import URI from "../../objects/uri";
+import { fetchStudentsDetails } from "../../actions/studentsDetails";
 
 class EditStudentProfile extends React.Component {
   state = {
@@ -29,10 +31,27 @@ class EditStudentProfile extends React.Component {
     // validate studentDetails
     const { error } = Joi.validate(this.state.student, studentDetailsSchema);
     if (error) {
+      console.log(error);
       return;
     }
     // send to server for update
     console.log(this.state.student);
+    fetch(`${URI}/api/student_details/${this.props.student.id}`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(this.state.student)
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(msg => {
+        console.log(msg);
+        if (msg == "OK") this.props.fetchStudentsDetails();
+      })
+      .catch(err => console.log(err.message));
   };
 
   render() {
@@ -62,5 +81,5 @@ export default connect(
     studentDetails: state.student.studentDetails,
     student: state.studentsDetails.filter(item => item.id == props.match.params.id)[0] || {}
   }),
-  { updateStudentDetails, resetStudent }
+  { updateStudentDetails, resetStudent, fetchStudentsDetails }
 )(EditStudentProfile);
