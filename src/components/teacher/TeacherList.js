05ea554d664ba22c6cb7a9322db2952ea99a6cd3
raihan <r_firoz@bitbucket.org>
2019-08-24 14:47:08 +0600

    teacher list

--- src/components/teacher/TeacherList.js
+++ src/components/teacher/TeacherList.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import { Card, CardHeader, CardBody, Col, Row } from "reactstrap";
 import List from "../common/List";
+import URI from "../../objects/uri";
 
-const teacherData = [
-  {
-    id: "29067",
-    name: "Raihan",
-    email: "test@email.com",
-    mobileNo: "67592746873",
-    subject: "English",
-    gender: "Male"
-  }
-];
+const listHeaders = {
+  id: "ID",
+  name: "Full Name",
+  mobileNo: "Mobile No",
+  email: "Email",
+  subject: "Subject",
+  gender: "Gender"
+};
+
+const headersAllow = ["id", "name", "mobileNo", "email", "subject", "gender"];
+
 const buttons = [
   {
     name: "View",
@@ -21,15 +23,48 @@ const buttons = [
   {
     name: "Edit",
     color: "warning",
-    link: "/teachers/entry"
+    link: "/teachers/edit"
   },
   {
     name: "Delete",
-    color: "danger"
+    color: "danger",
+    onClick: "delete"
   }
 ];
 
 class TeacherList extends Component {
+  state = { teachers: [] };
+
+  componentDidMount() {
+    fetch(`${URI}/api/teachers`, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => {
+        if (response.status == 200) return response.json();
+        return response.text();
+      })
+      .then(msg => {
+        console.log(msg);
+        if (typeof msg == "object") this.setState({ teachers: msg });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  deleteStudent = id => {
+    fetch(`${URI}/api/teachers/delete/${id}`, {
+      method: "POST",
+      mode: "cors"
+    })
+      .then(response => response.text())
+      .then(msg => {
+        console.log(msg);
+        if (msg == "ERROR") return;
+        this.setState({ teachers: this.state.teachers.filter(t => t.id != id) });
+      })
+      .catch(err => console.log(err.message));
+  };
+
   render() {
     return (
       <div className="animated fadeIn">
@@ -40,7 +75,13 @@ class TeacherList extends Component {
                 <strong>Teachers List</strong> <i className="icon-list icons" />
               </CardHeader>
               <CardBody>
-                <List data={teacherData} buttons={buttons} />
+                <List
+                  data={this.state.teachers}
+                  headerNames={listHeaders}
+                  headersAllow={headersAllow}
+                  buttons={buttons}
+                  delete={this.deleteStudent}
+                />
               </CardBody>
             </Card>
           </Col>
