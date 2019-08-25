f334e629fe214e4f467c1f38cc0ae8fee6b948a6
raihan <r_firoz@bitbucket.org>
2019-08-25 16:16:09 +0600

    user list, view and delete

--- src/components/user/UesrList.js
+++ src/components/user/UesrList.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Card, CardHeader, CardBody, Col, Row } from "reactstrap";
 import List from "../common/List";
+import URI from "../../objects/uri";
 
-const userData = [
-  {
-    id: "29067",
-    name: "Raihan",
-    email: "test@email.com",
-    mobileNo: "67592746873"
-  }
-];
+const listHeaders = {
+  id: "ID",
+  username: "Username",
+  email: "Email"
+};
+const headersAllow = ["id", "username", "email"];
+
 const buttons = [
   {
     name: "View",
@@ -18,25 +18,67 @@ const buttons = [
   },
   {
     name: "Delete",
-    color: "danger"
+    color: "danger",
+    onClick: "delete"
   }
 ];
 
-export default props => {
-  return (
-    <div className="animated fadeIn">
-      <Row>
-        <Col>
-          <Card>
-            <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
-              <strong>User List</strong> <i className="icon-list icons" />
-            </CardHeader>
-            <CardBody>
-              <List data={userData} buttons={buttons} />
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+class UserList extends React.Component {
+  state = { users: [] };
+
+  componentDidMount() {
+    fetch(`${URI}/api/users`, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => {
+        if (response.status == 200) return response.json();
+        return response.text();
+      })
+      .then(msg => {
+        console.log(msg);
+        if (typeof msg == "object" && msg.length) this.setState({ users: msg });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  deleteUser = id => {
+    fetch(`${URI}/api/users/delete/${id}`, {
+      method: "POST",
+      mode: "cors"
+    })
+      .then(response => response.text())
+      .then(msg => {
+        console.log(msg);
+        if (msg == "OK") this.props.history.push("/users/all");
+      })
+      .catch(err => console.log(err.message));
+  };
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
+                <strong>Users List</strong> <i className="icon-list icons" />
+              </CardHeader>
+              <CardBody>
+                <List
+                  data={this.state.users}
+                  headerNames={listHeaders}
+                  headersAllow={headersAllow}
+                  buttons={buttons}
+                  delete={this.deleteUser}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default UserList;
--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -1,49 +1,73 @@
 import React from "react";
 import { Button, Card, CardHeader, CardBody, Col, Row } from "reactstrap";
+import URI from "../../objects/uri";
 
-export default props => {
-  return (
-    <div className="animated fadeIn">
-      <Row>
-        <Col xs="4">
-          <Card>
-            <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
-              <strong>Saumik Nondi</strong> <i className="icon-user icons" />
-            </CardHeader>
-            <CardBody>
-              <Row>
-                <Col>ID</Col>
-                <Col>
-                  : <strong>1346345</strong>
-                </Col>
-              </Row>
-              <Row>
-                <Col>Name</Col>
-                <Col>
-                  : <strong>Raihan</strong>
-                </Col>
-              </Row>
-              <Row>
-                <Col>Email</Col>
-                <Col>
-                  : <strong>test@email.com</strong>
-                </Col>
-              </Row>
-              <Row>
-                <Col>Mobile No</Col>
-                <Col>
-                  : <strong>9034704096</strong>
-                </Col>
-              </Row>
-            </CardBody>
-          </Card>
-          <div className="quickLink">
-            <Button block color="danger">
-              Delete Profile
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+class UserProfile extends React.Component {
+  state = { user: {} };
+
+  componentDidMount() {
+    fetch(`${URI}/api/users/${this.props.match.params.id}`, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => {
+        if (response.status == 200) return response.json();
+        return response.text();
+      })
+      .then(msg => {
+        console.log(msg);
+        if (typeof msg == "object" && msg.length) this.setState({ user: msg[0] });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  deleteUser = () => {
+    fetch(`${URI}/api/users/delete/${this.state.user.id}`, {
+      method: "POST",
+      mode: "cors"
+    })
+      .then(response => response.text())
+      .then(msg => {
+        console.log(msg);
+        if (msg == "OK") this.props.history.push("/users/all");
+      })
+      .catch(err => console.log(err.message));
+  };
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xs="4">
+            <Card>
+              <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
+                <strong>{this.state.user.username}</strong> <i className="icon-user icons" />
+              </CardHeader>
+              <CardBody>
+                <Row>
+                  <Col>ID</Col>
+                  <Col>
+                    : <strong>{this.state.user.id}</strong>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>Email</Col>
+                  <Col>
+                    : <strong>{this.state.user.email}</strong>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+            <div className="quickLink">
+              <Button block color="danger" onClick={this.deleteUser}>
+                Delete Profile
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
