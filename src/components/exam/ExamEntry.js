52d1b4d4f8b3d76fdc17f6ac6e5fa622ee01de4c
raihan <r_firoz@bitbucket.org>
2019-09-02 11:14:32 +0600

    exams entry, list and delete

--- src/components/exam/ExamEntry.js
+++ src/components/exam/ExamEntry.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button, Card, CardHeader, CardBody, Col, Row } from "reactstrap";
+import Joi from "@hapi/joi";
 import FormField from "../common/FormField";
+import URI from "../../objects/uri";
 
 export default props => {
-  const [state, setState] = React.useState({});
+  const [state, setState] = React.useState({
+    error: ""
+  });
 
   const changeHandler = e => {
     setState({
@@ -15,6 +19,30 @@ export default props => {
   const clickHandler = () => {
     console.log(state);
     // validate and send to server
+    let error = Joi.validate(state.name, Joi.string().required()).error;
+    if (error) {
+      setState({
+        ...state,
+        error: '"Exam Name" is required'
+      });
+      return;
+    }
+    fetch(`${URI}/api/exams`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ name: state.name })
+    })
+      .then(response => response.text())
+      .then(msg => {
+        console.log(msg);
+        if (msg == "OK") {
+          props.history.push("/exams/all");
+        }
+      })
+      .catch(err => console.log(err.message));
   };
 
   return (
@@ -29,47 +57,12 @@ export default props => {
               <Row>
                 <Col>
                   <FormField
-                    type="date"
-                    placeholder="Date"
-                    name="date"
-                    value={state.date}
-                    onChange={changeHandler}
-                  />
-                  <FormField
                     type="text"
-                    placeholder="Class *"
-                    name="class"
-                    value={state.class}
-                    onChange={changeHandler}
-                  />
-                  <FormField
-                    type="text"
-                    placeholder="Group"
-                    name="group"
-                    value={state.group}
-                    onChange={changeHandler}
-                    disabled={state.class != "9" && state.class != "10"}
-                  />
-                  <FormField
-                    type="text"
-                    placeholder="Subject *"
-                    name="subject"
-                    value={state.subject}
-                    onChange={changeHandler}
-                  />
-                  <FormField
-                    type="text"
-                    placeholder="Exam Name"
+                    placeholder="Exam Name *"
                     name="name"
                     value={state.name}
                     onChange={changeHandler}
-                  />
-                  <FormField
-                    type="text"
-                    placeholder="Exam Description"
-                    name="description"
-                    value={state.description}
-                    onChange={changeHandler}
+                    error={state.error}
                   />
                   <Button block color="primary" onClick={clickHandler}>
                     Submit
--- src/components/exam/ExamList.js
+++ src/components/exam/ExamList.js
@@ -1,32 +1,18 @@
 import React from "react";
 import { Card, CardHeader, CardBody, Col, Row } from "reactstrap";
 import List from "../common/List";
+import URI from "../../objects/uri";
 
-const examData = [
-  {
-    id: "29067429867473",
-    class: "Five",
-    group: "",
-    subject: "English",
-    name: "Exam Name",
-    description: "Description",
-    date: "12/7/2019"
-  }
-];
+const headerNames = {
+  id: "ID",
+  name: "Name"
+};
+const headersAllow = ["id", "name"];
 const buttons = [
   {
-    name: "View",
-    color: "success",
-    link: "/exams/details"
-  },
-  {
-    name: "Edit",
-    color: "warning",
-    link: "/exams/entry"
-  },
-  {
     name: "Delete",
-    color: "danger"
+    color: "danger",
+    onClick: "delete"
   },
   {
     name: "Marks",
@@ -35,21 +21,72 @@ const buttons = [
   }
 ];
 
-export default props => {
-  return (
-    <div className="animated fadeIn">
-      <Row>
-        <Col>
-          <Card>
-            <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
-              <strong>Exam List</strong> <i className="icon-list icons" />
-            </CardHeader>
-            <CardBody>
-              <List data={examData} buttons={buttons} />
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+class ExamList extends React.Component {
+  state = { data: [] };
+
+  componentDidMount() {
+    this.fetchExams();
+  }
+  fetchExams = () => {
+    fetch(`${URI}/api/exams`, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => {
+        if (response.status == 200) return response.json();
+        else return response.text();
+      })
+      .then(msg => {
+        console.log(msg);
+        if (typeof msg == "object") this.setState({ data: msg });
+      })
+      .catch(err => console.log(err.message));
+  };
+
+  deleteExam = id => {
+    console.log(id);
+    fetch(`${URI}/api/exams/delete/`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ id })
+    })
+      .then(response => response.text())
+      .then(msg => {
+        console.log(msg);
+        this.fetchExams();
+      })
+      .catch(err => console.log(err.message));
+  };
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader style={{ display: "flex", justifyContent: "space-between" }}>
+                <strong>Exam List</strong> <i className="icon-list icons" />
+              </CardHeader>
+              <CardBody>
+                <List
+                  data={this.state.data}
+                  headerNames={headerNames}
+                  headersAllow={headersAllow}
+                  buttons={buttons}
+                  delete={id => {
+                    this.deleteExam(id);
+                  }}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ExamList;
