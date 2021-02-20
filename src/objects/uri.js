6bbec3c915d3a70a1d207482e2d3272061c9cad6
raihan <5421449+ra89fi@users.noreply.github.com>
2021-02-20 22:50:55 +0600

    github init

--- src/objects/uri.js
+++ src/objects/uri.js
@@ -1,2 +1,2 @@
 // add production backend URL here
-export default (process.env.NODE_ENV === "development" ? "http://localhost:3001" : "");
+export default process.env.NODE_ENV === "development" ? "http://localhost:3001" : "";
